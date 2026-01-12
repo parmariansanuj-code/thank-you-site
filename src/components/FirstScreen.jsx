25b475cc2df6d5772441c45f0b702d1d9ddf5b6f
parmariansanuj-code <parmariansanuj@gmail.com>
2026-01-12 20:20:38 +0530

    Update FirstScreen.jsx

--- src/components/FirstScreen.jsx
+++ src/components/FirstScreen.jsx
@@ -64,7 +64,7 @@ export default function FirstScreen({ onNext }) {
                     transition={{ duration: 5, repeat: Number.POSITIVE_INFINITY }}
                     style={{ backgroundSize: "200% 200%" }}
                 >
-                   Shreepada Roy, I want to say something
+                   bahan ke bhosade Ankit, I want to say something
                 </motion.h1>
                 <motion.p
                     className="text-2xl md:text-3xl font-bold text-gray-700 flex items-center justify-center gap-2"
